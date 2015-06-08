b148e0927af662d6cbd83417f2616a10c560f049
mikemey <miguel.maestro@gmx.co.uk>
2015-06-08 15:34:19 +0200

    moved client to package.

--- src/main/scala/client/PubQuizClient.scala
+++ src/main/scala/client/PubQuizClient.scala
@@ -1,10 +1,12 @@
-import akka.actor.{ActorRef, Actor, ActorSystem, Props}
+package client
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 
 object PubQuizClient {
 
   def main(args: Array[String]): Unit = {
-    val system = ActorSystem("client", ConfigFactory.load("application"))
+    val system = ActorSystem("client", ConfigFactory.load("client-application"))
     val clientActor: ActorRef = system.actorOf(Props[PubQuizClient], "client")
     clientActor ! "send"
   }
@@ -14,8 +16,8 @@ class PubQuizClient extends Actor {
   def receive = {
     case "send" =>
       println("sending connection message")
-      val maria = context.actorSelection("akka.tcp://maria@192.168.1.98:2552/user/orchestrator")
+      val maria = context.actorSelection("akka.tcp://maria@192.168.1.70:2552/user/orchestrator")
       maria ! "I put the ball in the net!!"
-    case resp: String => println(resp)
+    case resp: String => println("received: " + resp)
   }
 }
