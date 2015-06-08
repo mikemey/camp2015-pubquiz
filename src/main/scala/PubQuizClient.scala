919199c255e732b44fe681ac936614593709ed1a
mikemey <miguel.maestro@gmx.co.uk>
2015-06-08 15:13:23 +0200

    first draft client actor

---
+++ src/main/scala/PubQuizClient.scala
@@ -0,0 +1,21 @@
+import akka.actor.{ActorRef, Actor, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+object PubQuizClient {
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("client", ConfigFactory.load("application"))
+    val clientActor: ActorRef = system.actorOf(Props[PubQuizClient], "client")
+    clientActor ! "send"
+  }
+}
+
+class PubQuizClient extends Actor {
+  def receive = {
+    case "send" =>
+      println("sending connection message")
+      val maria = context.actorSelection("akka.tcp://maria@192.168.1.98:2552/user/orchestrator")
+      maria ! "I put the ball in the net!!"
+    case resp: String => println(resp)
+  }
+}
